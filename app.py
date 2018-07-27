c7faa5574e9a1e80bf49ec52839c1639388abf89
mattwong98 <matt.wong.1998@gmail.com>
2018-07-27 09:40:28 -0400

    added search to app.py

--- app.py
+++ app.py
@@ -31,6 +31,30 @@ def hello():
     return ','.join(str(i) for i in arr)
     db_session.remove()
 
+def search(filters):
+    #search returns a list of accuracy percentages for each beer based on a set of filters that are passed in
+    sql = """SELECT * from home_beer"""
+    result = db.engine.execute(sql)
+    beers = []
+    accuracies = []
+    for row in result:
+        beers.append(row)
+
+    numFilters = 0
+    for filter in filters:
+        if filter != "":
+            numFilters+=1
+
+    for beer in beers:
+        for filter in filters:
+            i = 0
+            accuracy = 0
+            if beer[i] == filter and filter!="":
+                accuracy+=1
+            accuracies.append(accuracy/numFilters)
+
+    return accuracies
+
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
