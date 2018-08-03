fb38a6f43a65f3ef101d9bec0a2b310b1d719897
mattwong98 <matt.wong.1998@gmail.com>
2018-08-03 14:14:02 -0400

    separated functions from main app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 # from models includes db
 from models import *
+from functions import searchFunc
 app = Flask(__name__)
 
 POSTGRES = {
@@ -31,7 +32,7 @@ def hello():
     return ','.join(str(i) for i in arr)
     db_session.remove()
 
-
+@app.route('/search/results')
 def search(filters):
     #search section
     sql = """SELECT * from home_beer"""
@@ -40,52 +41,13 @@ def search(filters):
     for row in result:
         beers.append(row)
 
-    accuracies = []
-    #accuracies is a list of the percentage matching to the filters
-    #if beer #2 of 4 matches 3/5 of the filters passed in,
-    #accuracies will be [0,0.6,0,0]
-
-    numFilters = 0
-    for filter in filters:
-        if filter != "":
-            numFilters+=1
-
-    for beer in beers:
-        #for now, beer.containerType and beer.taste do not exist
-        i = 0
-        accuracy = 0
-        if beer.alcoholVolume == filters[0] and filters[0]!="":
-            accuracy+=1
-        if beer.brand == filters[1] and filters[1]!="":
-            accuracy+=1
-        if beer.bodyType == filters[2] and filters[2] != "":
-            accuracy+=1
-        if beer.containerType == filters[3] and filters[3] != "":
-            accuracy+=1
-        if beer.taste == filters[4] and filters[4] != "":
-            accuracy+=1
-        accuracies.append(accuracy/numFilters)
-
-    #sort section
-    high = 1
-    sortedBeers = []
-
-    #for each level of accuracy, from high to low,
-    #checks all beers to see if it matches the level of accuracy and appends to sortedBeers
-    #upgrade would be to skip over beers that are of a higher accuracy
-    for i in range(numFilters,-1,-1):
-        j = 0
-        for beer in beers:
-            if accuracies[j] == i/numFilters:
-                sortedBeers.append(beer)
-
-    return sortedBeers
+    return searchFunc(beers, filters)
     db_session.remove()
 
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+@app.route('/<name>+<color>')
+def hello_name(name, color):
+    return "Hello {}{}!".format(color, name)
 
 @app.route('/test')
 def test():
---
+++ functions.py
@@ -0,0 +1,41 @@
+def searchFunc(beers, filters):
+    accuracies = []
+    # accuracies is a list of the percentage matching to the filters
+    # if beer #2 of 4 matches 3/5 of the filters passed in,
+    # accuracies will be [0,0.6,0,0]
+
+    numFilters = 0
+    for filter in filters:
+        if filter != "":
+            numFilters += 1
+
+    for beer in beers:
+        # for now, beer.containerType and beer.taste do not exist
+        i = 0
+        accuracy = 0
+        if beer.alcoholVolume == filters[0] and filters[0] != "":
+            accuracy += 1
+        if beer.brand == filters[1] and filters[1] != "":
+            accuracy += 1
+        if beer.bodyType == filters[2] and filters[2] != "":
+            accuracy += 1
+        if beer.containerType == filters[3] and filters[3] != "":
+            accuracy += 1
+        if beer.taste == filters[4] and filters[4] != "":
+            accuracy += 1
+        accuracies.append(accuracy / numFilters)
+
+    # sort section
+    high = 1
+    sortedBeers = []
+
+    # for each level of accuracy, from high to low,
+    # checks all beers to see if it matches the level of accuracy and appends to sortedBeers
+    # upgrade would be to skip over beers that are of a higher accuracy
+    for i in range(numFilters, -1, -1):
+        j = 0
+        for beer in beers:
+            if accuracies[j] == i / numFilters:
+                sortedBeers.append(beer)
+
+    return sortedBeers, accuracies
