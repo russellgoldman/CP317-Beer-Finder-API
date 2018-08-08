b275d6545d50b538c30316ed444c780112dca431
Kevin Tang <kwh_tang@hotmail.com>
2018-08-08 12:58:04 -0400

    Fix problem in the search function

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
 def searchFunc(beers, filters):
-    accuracies = []
+    sortedBeers = []
     # accuracies is a list of the percentage matching to the filters
     # if beer #2 of 4 matches 3/5 of the filters passed in,
     # accuracies will be [0,0.6,0,0]
@@ -13,8 +13,8 @@ def searchFunc(beers, filters):
         # for now, beer.containerType and beer.taste do not exist
         i = 0
         accuracy = 0
-        if beer.alcoholVolume == filters[0] and filters[0] != "":
-            accuracy += 1
+        if filters[0] != "":
+            accuracy += 1 - ((abs(int(filter[0])-int(beer.alcoholVolume)))/int(filter[0]))
         if beer.brand == filters[1] and filters[1] != "":
             accuracy += 1
         if beer.bodyType == filters[2] and filters[2] != "":
@@ -23,22 +23,29 @@ def searchFunc(beers, filters):
             accuracy += 1
         if beer.taste == filters[4] and filters[4] != "":
             accuracy += 1
-        accuracies.append(accuracy / numFilters)
-
+        sortedBeers.append([beer, accuracy/numFilters])
     # sort section
-    high = 1
-    sortedBeers = []
+
 
     # for each level of accuracy, from high to low,
     # checks all beers to see if it matches the level of accuracy and appends to sortedBeers
     # upgrade would be to skip over beers that are of a higher accuracy
-    for i in range(numFilters, -1, -1):
-        j = 0
-        for beer in beers:
-            if accuracies[j] == i / numFilters:
-                sortedBeers.append(beer)
 
+    insertionSort(sortedBeers)
     return sortedBeers, accuracies
+
+def insertionSort(alist):
+   for index in range(1,len(alist)):
+
+     currentvalue = alist[index]
+     position = index
+
+     while position>0 and alist[position-1][1]>currentvalue[1]:
+         alist[position]=alist[position-1]
+         position = position-1
+
+     alist[position]=currentvalue
+   return
 """
 -------------------------------------------------------
 name: top_picksFunc
